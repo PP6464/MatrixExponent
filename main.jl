b2228cf1072f976b9f7276ff8779654aeddf4de4
Panth Patel <panthhp@outlook.com>
2024-03-06 20:13:38 +0000

    exponentiate matrices

--- main.jl
+++ main.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra
 
-function matrix_exp(M::Matrix{Number})
+function matrix_exp(M::Matrix{T}) where T <: Number
   # Throw error if matrix is not square
   if size(M)[1] != size(M)[2]
     throw(ArgumentError("The matrix must be square"))
@@ -13,7 +13,7 @@ function matrix_exp(M::Matrix{Number})
     end
     for j ∈ 1:size(M)[2]
       if i != j && M[i, j] != 0
-        is_diagonal = true
+        is_diagonal = false
         break
       end
     end
@@ -22,7 +22,7 @@ function matrix_exp(M::Matrix{Number})
   if is_diagonal
     res_mat = zeros(Number, size(M)[1], size(M)[2])
     for i ∈ 1:size(M)[1]
-      res_mat[i, i] = exp(M[i, i])
+      res_mat[i, i] = ℯ^M[i, i]
     end
     return res_mat
   end
@@ -30,27 +30,25 @@ function matrix_exp(M::Matrix{Number})
   eigen_info = eigen(M)
   if length(unique(eigen_info.values)) == size(M)[1]
     # There are sufficient eigenvalues
-    eigenvalues = zeros(Number, size(M)[1], size(M)[1])
-    for i ∈ 1:eigen_info.values
-      eigenvalues[i, i] = eigen_info.values[i]
+    eigenvalues = zeros(Number, size(M)[1], size(M)[2])
+    for (i, v) ∈ enumerate(eigen_info.values)
+      eigenvalues[i, i] = v
     end
     return eigen_info.vectors * matrix_exp(eigenvalues) * inv(eigen_info.vectors)
   end
   # Check if matrix is nilpotent
-  k = 1
-  current_mat = M
-  while true
-    if M * current_mat == zeros(Number, size(M)[1], size(M)[2])
-      # The matrix is nilpotent with nilpotency degree k
-      res = zero(Number)
-      for i ∈ 1:k - 1
-        res += M^k / factorial(i)
+  k = 0
+  M_powers = [Matrix{Int64}(I, size(M)[1], size(M)[2])]
+  while k < size(M)[1]
+    push!(M_powers, M_powers[end] * M)
+    k += 1
+    if M_powers[end] == zeros(ComplexF64, size(M)[1], size(M)[2])
+      println("nilpotent")
+      # M is nilpotent with degree k
+      res_mat = zeros(ComplexF64, size(M)[1], size(M)[2])
+      for i ∈ 1:k+1
+        res_mat += M_powers[i] / factorial(i - 1)
       end
-    else if k <= size(M)[1]
-      k += 1
-      current_mat = M * current_mat
-    else
-      break
     end
   end
   # Return default matrix exponent function (uses approximation)
